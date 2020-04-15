e1d46252ddc4b96686c3ce2fd11b73f8bb9476aa
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-04-15 14:34:40 +1000

    Proposed new test structure

--- getting-started/src/test/java/org/acme/getting/started/GreetingResourceTest.java
+++ getting-started/src/test/java/org/acme/getting/started/GreetingResourceTest.java
@@ -1,10 +1,10 @@
 package org.acme.getting.started;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
-
 import java.util.UUID;
 
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -12,24 +12,18 @@ import io.quarkus.test.junit.QuarkusTest;
 @QuarkusTest
 public class GreetingResourceTest {
 
+    @Inject
+    GreetingResource greetingResource;
+
     @Test
     public void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("hello"));
+        Assertions.assertEquals("hello", greetingResource.hello());
     }
 
     @Test
     public void testGreetingEndpoint() {
         String uuid = UUID.randomUUID().toString();
-        given()
-                .pathParam("name", uuid)
-                .when().get("/hello/greeting/{name}")
-                .then()
-                .statusCode(200)
-                .body(is("hello " + uuid));
+        Assertions.assertEquals("hello " + uuid, greetingResource.greeting(uuid));
     }
 
 }
